8c89bbb8c6ed8c80afab6842bfc1b9359dbf8ee7
Tim Dawson <tim@ghoststreet.co.nz>
2014-12-31 17:28:00 +1300

    switched from Grunt to Gulp build script. Hello 2015

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ feed.reconfine.com
 Source code for the feed.reconfine.com Tumblr theme.
 Free for personal re-use
 
+Uses [Gulp](http://gulpjs.com) for code compilation.
+
 ###Coming Next
 
 - search page template for 0 results
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+gulp.task('styles', function() {
+  return gulp.src('src/less/styles.less')
+    .pipe(plugins.less())
+    .pipe(plugins.autoprefixer())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(plugins.rename({suffix: '.min'}))
+    .pipe(plugins.cssmin())
+    .pipe(gulp.dest('dist/css'))
+    //.on('error', gutil.log)
+    .pipe(plugins.notify({ message: 'Styles Task Complete' }));
+});
+
+gulp.task ('scripts', function() {
+  return gulp.src(['src/vendor/js/*.js','src/js/*.js'])
+    .pipe(plugins.concat('app.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(plugins.rename({suffix: '.min'}))
+    .pipe(plugins.uglify())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(plugins.notify({ message: 'Scripts Task Complete' }));
+});
+
+gulp.task('watch', function() {
+  plugins.livereload.listen();
+  gulp.watch('src/js/*.js', ['scripts']);
+  gulp.watch('src/less/*.less',['styles']);
+  gulp.watch(['dist/**']).on('change',plugins.livereload.changed);
+});
